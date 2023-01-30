a3200b47d79de39f9b9472b4f46f37f009b75b67
guidovitale <g.vitale@contralto-audio.com>
2023-01-30 19:30:13 +0100

    json reading

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,38 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  let jsonString = 'empty';
+  //let fn = 'https://dummyjson.com/docs/products/1';
+  //let fn = 'https://jsonplaceholder.typicode.com/';
+  let fn = './dummy.json'
+
+  function visJson(filename){
+    fetch(filename).then(res => res.json()).then(console.log);
+    let promise = fetch(filename).then(res => res.json());
+    promise.then((res)=>{
+      jsonString = JSON.stringify(res, null, '\n');
+    })
+  }
+
+  function onClick()
+  {
+    jsonString = 'waiting...';
+    setTimeout(function(){visJson(fn)}, 2000);
+  }
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
+  <h1>Example</h1>
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    This is the loaded json:
   </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+  <p>
+    {jsonString}
   </p>
 </main>
 
+<button on:click={onClick}>
+	Click to load and visualize
+</button>
+
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
