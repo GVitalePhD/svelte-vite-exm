39e17fb3ba742dcf84ab8da057c3a0b364145c66
guidovitale <g.vitale@contralto-audio.com>
2023-01-31 11:24:15 +0100

    better flow

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,7 @@
 <script>
-  let jsonString = 'empty';
-  //let fn = 'https://dummyjson.com/docs/products/1';
-  //let fn = 'https://jsonplaceholder.typicode.com/';
-  let fn = './dummy.json'
+  import Loader from "./Loader.svelte";
 
-  function visJson(filename){
-    fetch(filename).then(res => res.json()).then(console.log);
-    let promise = fetch(filename).then(res => res.json());
-    promise.then((res)=>{
-      jsonString = JSON.stringify(res, null, '\n');
-    })
-  }
-
-  function onClick()
-  {
-    jsonString = 'waiting...';
-    setTimeout(function(){visJson(fn)}, 2000);
-  }
-
+  let show = false;
 </script>
 
 <main>
@@ -25,14 +9,27 @@
   <p>
     This is the loaded json:
   </p>
-  <p>
-    {jsonString}
-  </p>
+  
+  <button on:click={ () => show = !show }>
+    Show Component</button>
+    {#if show}
+      <!-- <p> {visJson()} </p>  
+      <p> {jsonString} </p> -->
+      <Loader />
+    {:else}
+      <p> no data!! </p>
+    {/if}
 </main>
 
-<button on:click={onClick}>
+<!-- <button on:click={onClick}>
 	Click to load and visualize
 </button>
+function onClick()
+      {
+        jsonString = 'waiting...';
+        setTimeout(visJson, 2000);
+      }
+ -->
 
 <style>
 </style>
---
+++ src/Loader.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {onMount, onDestroy} from 'svelte'
+    
+      let jsonString = 'empty';
+      let promise;
+      //let fn = 'https://dummyjson.com/docs/products/1';
+      //let fn = 'https://jsonplaceholder.typicode.com/';
+      const filename = './dummy.json'
+    
+      onMount(async () => {
+        console.log('activating the loader');
+        promise = fetch(filename)
+            .then(res => res.json())
+            .then((res)=>{jsonString = JSON.stringify(res, null, '\n');})
+        });
+      
+      onDestroy(() => {console.log('closing the loader');});
+</script>
+    
+<p> {jsonString} </p>
