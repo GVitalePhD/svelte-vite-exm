b0a8da0b686bed919e049fa6b5dbf9bf32d16af2
guidovitale <g.vitale@contralto-audio.com>
2023-01-31 14:24:41 +0100

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -13,23 +13,11 @@
   <button on:click={ () => show = !show }>
     Show Component</button>
     {#if show}
-      <!-- <p> {visJson()} </p>  
-      <p> {jsonString} </p> -->
       <Loader />
     {:else}
       <p> no data!! </p>
     {/if}
 </main>
-
-<!-- <button on:click={onClick}>
-	Click to load and visualize
-</button>
-function onClick()
-      {
-        jsonString = 'waiting...';
-        setTimeout(visJson, 2000);
-      }
- -->
 
 <style>
 </style>
